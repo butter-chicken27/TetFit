9da05ca9cae9652260a438754216a8970e652373
Tarun Ram <tarun.starfire@gmail.com>
2020-08-21 20:24:20 +0530

    Fixed the updateRating issue while posting to server

--- app/src/main/java/com/example/tetfit/exAdapter.java
+++ app/src/main/java/com/example/tetfit/exAdapter.java
@@ -3,7 +3,9 @@ package com.example.tetfit;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class exAdapter extends ArrayAdapter<Ex_short> {
@@ -23,11 +30,12 @@ public class exAdapter extends ArrayAdapter<Ex_short> {
         super(context, 0 , exercises);
     }
 
+    public Ex_short ex;
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        final Ex_short ex = getItem(position);
+        ex = getItem(position);
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.exec_customise, parent, false);
         }
@@ -67,6 +75,11 @@ public class exAdapter extends ArrayAdapter<Ex_short> {
                         String rating = input.getText().toString();
                         int rate_value = Integer.parseInt(rating);
                         String title = ex.get_title();
+
+                        Integer[] params = {rate_value};
+
+                        new updateRating().execute(params);
+
                     }
                 });
                 builder.show();
@@ -74,4 +87,59 @@ public class exAdapter extends ArrayAdapter<Ex_short> {
         });
         return convertView;
     }
+
+    private class updateRating extends AsyncTask<Integer,Void,String> {
+
+        String updateEndpoint = "http://18.188.175.235/exercises/updateRating";
+        @Override
+        protected String doInBackground(Integer... integers) {
+            int newRating = integers[0];
+            Log.e("UPDATERATING", "doInBackground: "+ex.get_title() );
+            String jsonRequestString = String.format("{\"name\":\"%s\",\"newRating\":%d}",ex.get_title(),newRating);
+
+            try {
+                URL obj = new URL(updateEndpoint);
+
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type","application/json");
+
+                con.setDoOutput(true);
+                OutputStream os = con.getOutputStream();
+                byte[] input = jsonRequestString.getBytes("UTF-8");
+                os.write(input,0,input.length);
+                os.flush();
+                os.close();
+
+                int responseCode = con.getResponseCode();
+
+                if(responseCode==HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader((con.getInputStream())));
+                    String inputLine;
+
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    return response.toString();
+                } else {
+                    return null;
+                }
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s==null) {
+                Log.e("UPDATERATING", "onPostExecute: Error in Updating Rating");
+            } else {
+                Log.e("UPDATERATING", "onPostExecute: "+s );
+            }
+        }
+    }
+
 }
